4e348d75d1d8aaf5e8b48764a5a601cfffeba924
SoshinK <soshinkonstantinv@gmail.com>
2021-05-19 16:47:38 +0300

    ADD: dataset.py: added сutImplantRegion() and getBoundingBox() functions

--- autoimplant/dataset.py
+++ autoimplant/dataset.py
@@ -6,6 +6,9 @@ import nrrd
 
 from torch.utils.data import Dataset
 
+import numpy as np
+from skimage.morphology import opening
+from skimage.measure import label, regionprops
 
 class Autoimplant(Dataset):
     def __init__(self, root: str):
@@ -24,3 +27,53 @@ class Autoimplant(Dataset):
 
     def __len__(self):
         return len(list((self.root / 'complete_skull').glob('*/*.nrrd')))
+
+
+def сutImplantRegion(healthy_skull_predicted, corrupted_skull):
+    '''
+    Find voxel occupancy grid of skull implant
+
+    Parameters
+    ----------
+        healthy_skull_predicted: np.ndarray
+            Vovel occupancy grid with shape NxNxN of predicted skull without hole
+        corrupted_skull: np.ndarray
+            Vovel occupancy grid with shape NxNxN of skull with hole
+
+    Returns
+    -------
+        np.ndarray
+    '''
+    implant = healthy_skull_predicted - corrupted_skull
+    implant[implant < 0] = 0
+
+    implant = opening(implant.astype(np.uint8))
+
+    # https://stackoverflow.com/questions/53730029/get-biggest-coherent-area
+    l = label(implant)
+    implant = (l==np.bincount(l.ravel())[1:].argmax()+1).astype(int)
+
+    return implant
+
+def getBoundingBox(implant, lx=None, ly=None, lz=None):
+    l = label(implant)
+    r = regionprops(l)
+    min_x, min_y, min_z, max_x, max_y, max_z= r[0].bbox
+    if not lx is None:
+        if max_x - min_x > lx:
+            raise ValueError(f"Actual size of bounding box larger than is specified: expected {lx}, but given {max_x - min_x}")
+        min_x -= (lx - max_x + min_x) // 2
+        max_x += (lx - max_x + min_x) // 2 + (lx - max_x + min_x) % 2
+    if not ly is None:
+        if max_y - min_y > ly:
+            raise ValueError(f"Actual size of bounding box larger than is specified: expected {ly}, but given {max_y - min_y}")
+        min_y -= (ly - max_x + min_x) // 2
+        max_y += (ly - max_x + min_x) // 2 + (ly - max_x + min_x) % 2
+    if not lz is None:
+        if max_z - min_z > lz:
+            raise ValueError(f"Actual size of bounding box larger than is specified: expected {lz}, but given {max_z - min_z}")
+        min_z -= (lz - max_x + min_x) // 2
+        max_z += (lz - max_x + min_x) // 2 + (lz - max_x + min_x) % 2
+    return min_x, min_y, min_z, max_x, max_y, max_z
+
+
